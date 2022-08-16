2fc6f65c8cc305187ac80206d020cd001ed756fd
memed <ahmed.rafik@digiterio.fr>
2022-08-16 20:19:28 +0200

    add dotenv config

--- AuthService.js
+++ AuthService.js
@@ -1,17 +1,18 @@
 const axios = require('axios');
+require('dotenv').config()
 
 let AuthService = class {
 
     async login() {
-        await axios.post('http://localhost:3000/login', {
-                user: "BankinUser",
-                password: "12345678"
+        await axios.post(process.env.BASE_URL+'/login', {
+                user: process.env.USER_NAME,
+                password: process.env.PASSWORD
             },
             {
                 headers: {'Content-Type': 'application/json'},
                 auth: {
-                    username: "BankinClientId",
-                    password: "secret"
+                    username: process.env.CLIENT_ID,
+                    password: process.env.CLIENT_SECRET
                 }
             })
             .then(function (response) {
@@ -19,7 +20,7 @@ let AuthService = class {
                 return response.data;
             })
             .catch(function (error) {
-                console.log(error);
+                console.error(error);
             });
     }
 }
